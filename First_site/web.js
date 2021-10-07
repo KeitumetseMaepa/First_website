2deaffab2eea336ac0148eaa0fdb556cce4ffb44
KeitumetseMaepa <91154066+KeitumetseMaepa@users.noreply.github.com>
2021-10-07 11:49:16 +0200

    Moved files
    Moved all the files to my files to Github file.

---
+++ First_site/web.js
@@ -0,0 +1,12 @@
+// This is my first JavaScript code!
+console.log('All things coding');
+
+// performing a task
+function greet(name, lastname) {
+    console.log('All things coding' + name + ' ' + lastname)
+}
+greet('Micaela');
+greet('Zaid');
+
+<img src="After CSS.png" width="30%" alt="Picture of tags used when coding" align="center"/>
+<img src="Before CSS.png" width="47%" alt="Picture of tags used when coding" align="left"/>
